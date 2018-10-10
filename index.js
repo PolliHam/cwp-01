d55b60e53464229eaadb16b8c417959c70960071
PolliHam <polina.shenets@gmail.com>
2018-10-10 03:57:50 +0300

    add function accessFile()

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const fs = require('fs');
 const path = require('path');
-const dir_path = __dirname;
+const dir_path = process.argv[2];
 let text_script =
-    'const fs = require(\'fs\');\n'+
-    'const path = require(\'path\');\n'+
-    'const dir_path = __dirname;\n'+
-    'function parser(dir_path){\n'+
-    '    fs.readdir(dir_path, function(err, files) {\n'+
-    '        for (let i = 0; i < files.length; i++) {\n'+
-    '            analysis(dir_path + \'\\\\\' + files[i]);\n'+
-    '        }\n'+
-    '    })\n'+
-    '}\n'+
-    'function analysis(object_name){\n'+
-    '    fs.stat(object_name, (err, stats)=> {\n'+
-    '        if (stats.isDirectory()) {\n'+
-    '            parser(object_name);\n'+
-    '        }else{\n'+
-    '            console.log(path.relative(dir_path, object_name));\n'+
-    '        }\n'+
-    '    })\n'+
-    '}\n'+
-    'parser(dir_path);\n';
+    'const fs = require(\'fs\');\r\n'+
+    'const path = require(\'path\');\r\n'+
+    'const dir_path = __dirname;\r\n'+
+    'function parser(dir_path){\r\n'+
+    '    fs.readdir(dir_path, function(err, files) {\r\n'+
+    '        for (let i = 0; i < files.length; i++) {\r\n'+
+    '            analysis(dir_path + \'\\\\\' + files[i]);\r\n'+
+    '        }\r\n'+
+    '    })\r\n'+
+    '}\r\n'+
+    'function analysis(object_name){\r\n'+
+    '    fs.stat(object_name, (err, stats)=> {\r\n'+
+    '        if (stats.isDirectory()) {\r\n'+
+    '            parser(object_name);\r\n'+
+    '        }else{\r\n'+
+    '            console.log(path.relative(dir_path, object_name));\r\n'+
+    '        }\r\n'+
+    '    })\r\n'+
+    '}\r\n'+
+    'parser(dir_path);\r\n';
 
+function accessFile() {
+    fs.access(dir_path,(err)=>{
+        if(err){
+            console.log('Error in dir path');
+        }
+        else{
+            createFile();
+        }
+    });
+}
 function createFile(){
     let file_name = dir_path+'/summary.js';
     fs.writeFile(file_name, text_script, (err)=>{
@@ -31,4 +41,4 @@ function createFile(){
         }
     });
 }
-createFile();
+accessFile();
